017cf7c04f77562ad7b7847a6b534754816f1337
Tommalieh <muhammedtommalieh@gmail.com>
2020-05-19 02:06:14 +0300

    Done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
 let noteInput = new Input();
+let createdNote = new Notes(noteInput);
 
-Notes.execute(noteInput);
-Notes.add(noteInput);
+createdNote.execute(noteInput);
+createdNote.add(noteInput);
