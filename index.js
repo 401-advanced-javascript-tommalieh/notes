0fd62b462f22d9846333baf422b0bcfc9840f68b
Tommalieh <muhammedtommalieh@gmail.com>
2020-05-20 03:33:59 +0300

    Add database functionality

--- index.js
+++ index.js
@@ -6,5 +6,16 @@ const Notes = require('./lib/notes.js');
 let noteInput = new Input();
 let createdNote = new Notes(noteInput);
 
+if (noteInput.action === 'add') {
+  createdNote.add(noteInput);
+}
+
+else if (noteInput.action === 'list') {
+  createdNote.list(noteInput);
+}
+
+else if (noteInput.action === 'delete') {
+  createdNote.delete(noteInput);
+}
+
 createdNote.execute(noteInput);
-createdNote.add(noteInput);
